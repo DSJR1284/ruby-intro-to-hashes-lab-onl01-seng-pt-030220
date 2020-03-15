eee1ecc2a0430d1416acb2327651a0b8368e14ad
Doug Jones <dsjr1284@gmail.com>
2020-03-15 01:31:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,14 +7,15 @@ def actor
 end
 
 def monopoly
-	monopoly = {
-	  :railroad => {}
+	
 	}
 end
 
 def monopoly_with_second_tier
 		monopoly = {
-	  
+		:railroads => {pieces: 4, names: {}, rent_in_dollars: {}
+	}
+
 	}
 
 end
